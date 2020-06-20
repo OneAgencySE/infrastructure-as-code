5adde44eac58c31c1b1864faa3829a35a248f971
Mikael Paulsson <mikael.pal@gmail.com>
2020-06-20 23:23:52 +0200

    Added example of using arm templates to deploy act

--- arm-templates/deploy.ps1
+++ arm-templates/deploy.ps1
@@ -1,1 +1,10 @@
-New-AzResourceGroupDeployment -ResourceGroupName "oa-infra-dev" -TemplateFile ".\environment.json"
+$context = Get-AzContext
+if (!$context) {
+    Connect-AzAccount
+}
+
+Set-AzContext -Subscription "4ded1a6f-aef9-4141-a03b-5d74d2bb8519"
+
+New-AzResourceGroup -Name "oa-infra-dev" -Location "west europe"
+
+New-AzResourceGroupDeployment -ResourceGroupName "oa-infra-dev" -TemplateFile ".\environment.json" -TemplateParameterFile ".\parameters.json"
