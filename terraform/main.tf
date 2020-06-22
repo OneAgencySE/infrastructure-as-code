217c3deae825407d73324727f92f1f2812fb9c2f
Mikael Paulsson <mikael.pal@gmail.com>
2020-06-22 22:07:32 +0200

    added terraform example

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "oa-infra-dev"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "oainfraacrdev"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Basic"
+  admin_enabled            = false
+}
+
