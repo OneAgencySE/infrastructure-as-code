e05d57e52c7870385ae5967fdb67e6b8cfc82493
Mikael Paulsson <mikael.paulsson@oneagency.se>
2020-08-26 23:38:13 +0200

    terraform fmt formatting done

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,9 @@ provider "azurerm" {
   version = "=2.0.0"
 
   subscription_id = "4ded1a6f-aef9-4141-a03b-5d74d2bb8519"
-  client_id = "c9185292-73e4-4bf1-90a7-7c12d59a3d2a"
-  client_secret = var.client_secret
-  tenant_id = "db1271e1-985e-4b67-957d-910f6618d2b4"
+  client_id       = "c9185292-73e4-4bf1-90a7-7c12d59a3d2a"
+  client_secret   = var.client_secret
+  tenant_id       = "db1271e1-985e-4b67-957d-910f6618d2b4"
 
   features {}
 }
@@ -23,10 +23,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "oainfraacrdev"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  sku                      = "Basic"
-  admin_enabled            = false
+  name                = "oainfraacrdev"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
 
