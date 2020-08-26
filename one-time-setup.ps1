51e830b27dda8eea23aee549dbc1f8d620cdc311
Mikael Paulsson <mikael.paulsson@oneagency.se>
2020-08-26 22:33:06 +0200

    setting up terraform connection to azure

--- one-time-setup.ps1
+++ one-time-setup.ps1
@@ -2,7 +2,7 @@
 
 $Subscription = "4ded1a6f-aef9-4141-a03b-5d74d2bb8519"
 
-$account = SetupDeployAccount -AccountName "devops-tuesday-deploy-user-16" -Subscription $Subscription
+$account = SetupDeployAccount -AccountName "devops-tuesday-deploy-user" -Subscription $Subscription
 
 Write-Host($account)
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,19 @@
+variable "client_secret" {}
+
 terraform {
-  backend "remote" {
-    workspaces {
-      name = "infrastructure-as-code"
-    }
+  backend "local" {
+    path = "terraform/terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   version = "=2.0.0"
+
+  subscription_id = "4ded1a6f-aef9-4141-a03b-5d74d2bb8519"
+  client_id = "c9185292-73e4-4bf1-90a7-7c12d59a3d2a"
+  client_secret = var.client_secret
+  tenant_id = "db1271e1-985e-4b67-957d-910f6618d2b4"
+
   features {}
 }
 
