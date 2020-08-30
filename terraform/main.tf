48179fd51d1915f92ae8c21684a2c0617fa933cb
Mikael Paulsson <mikael.paulsson@oneagency.se>
2020-08-30 22:00:46 +0200

    terraform fmt changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 variable "subscription_id" {
-  type = string
+  type    = string
   default = "4ded1a6f-aef9-4141-a03b-5d74d2bb8519"
 }
 variable "tenant_id" {
-  type = string
+  type    = string
   default = "db1271e1-985e-4b67-957d-910f6618d2b4"
 }
 variable "client_id" {
-  type = string
+  type    = string
   default = "c9185292-73e4-4bf1-90a7-7c12d59a3d2a"
 }
 variable "client_secret" {
